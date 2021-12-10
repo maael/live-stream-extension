38c48e0ef7d132589b9f0db6a671e360b35cee42
Matt Elphick <matt.a.elphy@gmail.com>
2021-12-10 15:27:59 +0000

    feat: Handle importing better

--- src/background.js
+++ src/background.js
@@ -8,78 +8,158 @@ const INITIAL_FOLLOWS = [
 
 async function getLiveInfo() {
   return new Promise((resolve) => {
-    chrome.storage.sync.get(
-      "follows",
-      async ({ follows = INITIAL_FOLLOWS }) => {
-        console.info("follows", follows);
-        const params = new URLSearchParams();
-        const youtube = follows
-          .filter(({ type }) => type === "youtube")
-          .map((i) => i.channel)
-          .join(",");
-        const twitch = follows
-          .filter(({ type }) => type === "twitch")
-          .map((i) => i.channel)
-          .join(",");
-        if (youtube.length) params.append("youtube", youtube);
-        if (twitch.length) params.append("twitch", twitch);
-        const result = await fetch(
-          `https://discord-slash-commands.vercel.app/api/live?${params.toString()}`
-        )
-          .then(async (r) => {
-            if (r.ok) {
-              const { result: data } = await r.json();
-              return data;
-            } else {
-              console.error("[error]", r.status, r.statusText, await r.text());
-              return [];
-            }
-          })
-          .catch((e) => console.error("[error]", e));
-        return resolve(result);
-      }
-    );
+    syncGet("follows", async (follows = INITIAL_FOLLOWS) => {
+      console.info("follows", follows);
+      const params = new URLSearchParams();
+      const youtube = follows
+        .filter(({ type }) => type === "youtube")
+        .map((i) => i.channel)
+        .join(",");
+      const twitch = follows
+        .filter(({ type }) => type === "twitch")
+        .map((i) => i.channel)
+        .join(",");
+      if (youtube.length) params.append("youtube", youtube);
+      if (twitch.length) params.append("twitch", twitch);
+      const result = await fetch(
+        `https://discord-slash-commands.vercel.app/api/live?${params.toString()}`
+      )
+        .then(async (r) => {
+          if (r.ok) {
+            const { result: data } = await r.json();
+            return data;
+          } else {
+            console.error("[error]", r.status, r.statusText, await r.text());
+            return [];
+          }
+        })
+        .catch((e) => console.error("[error]", e));
+      return resolve(result);
+    });
   });
 }
+
+chrome.notifications.onClicked.addListener(function (notificationId) {
+  if (notificationId.includes("https://")) {
+    chrome.tabs.create({ active: true, url: notificationId });
+  }
+});
 
 async function getInfo() {
   const [liveInfo, existingInfo] = await Promise.all([
     getLiveInfo(),
-    chrome.storage.sync.get("liveInfo"),
+    asyncGet("liveInfo"),
   ]);
-  const { liveInfo: existing = [] } = existingInfo;
+  const existing = existingInfo || [];
   const gotLive = liveInfo.filter((i) => i.isLive);
   const existingLive = existing.filter((i) => i.isLive);
-  gotLive
-    .filter(
-      (g) =>
-        !existingLive.some((e) => e.channel === g.channel && e.type === g.type)
-    )
-    .forEach((g) => {
-      chrome.notifications.create({
+  const notifications = gotLive.filter(
+    (g) =>
+      !existingLive.some((e) => e.channel === g.channel && e.type === g.type)
+  );
+
+  if (notifications.length < 4) {
+    notifications.forEach((g) => {
+      chrome.notifications.create(g.link, {
         title: `${g.channel} is going live!`,
         type: "basic",
         message: "Channel going live now!",
         iconUrl: "/images/get_started16.png",
       });
     });
+  } else {
+    chrome.notifications.create({
+      title: `${notifications.length} channels going live!`,
+      type: "basic",
+      message: "Channels going live now!",
+      iconUrl: "/images/get_started16.png",
+    });
+  }
   console.info("[received]", { liveInfo });
-  chrome.storage.sync.set({ liveInfo });
+  asyncStore("liveInfo", liveInfo);
   chrome.runtime.sendMessage({ liveInfo });
 }
 
-chrome.runtime.onInstalled.addListener(async () => {
-  const existingFollows = await chrome.storage.sync.get("follows");
-  console.info("[existing:follows]", existingFollows);
-  if (!existingFollows) {
-    await chrome.storage.sync.set({ follows: INITIAL_FOLLOWS });
-    console.info("[set:follows]", INITIAL_FOLLOWS);
-  }
-  await chrome.storage.sync.set({ liveInfo: [] });
-  chrome.alarms.create("live-info", { delayInMinutes: 0.5 });
-  chrome.alarms.onAlarm.addListener(async (alarm) => {
-    if (alarm.name !== "live-info") return;
-    await getInfo();
-  });
+chrome.alarms.onAlarm.addListener(async (alarm) => {
+  if (alarm.name !== "live-info") return;
   await getInfo();
 });
+
+chrome.runtime.onInstalled.addListener(async () => {
+  const existingFollows = await asyncGet("follows");
+  console.info("[existing:follows]", existingFollows);
+  if (!existingFollows) {
+    await asyncStore("follows", INITIAL_FOLLOWS);
+    console.info("[set:follows]", INITIAL_FOLLOWS);
+  }
+  await asyncStore("liveInfo", []);
+  const existingAlarm = await chrome.alarms.get("live-info");
+  if (!existingAlarm) {
+    chrome.alarms.create("live-info", { delayInMinutes: 1 });
+  }
+  await getInfo();
+});
+
+function asyncStore(key, objectToStore) {
+  return new Promise((resolve) => syncStore(key, objectToStore, resolve));
+}
+
+function asyncGet(key) {
+  return new Promise((resolve) => syncGet(key, resolve));
+}
+
+function syncStore(key, objectToStore, callback) {
+  const jsonstr = JSON.stringify(objectToStore);
+  const storageObj = {};
+  const blockSize =
+    chrome.storage.sync.QUOTA_BYTES_PER_ITEM / 2 - (4 + key.length);
+  const numberOfBlocks = Math.ceil(jsonstr.length / blockSize);
+  console.info("[syncStore:start]", {
+    key,
+    numberOfBlocks,
+    blockSize,
+    itemSize: jsonstr.length,
+  });
+
+  for (let i = 0; i < numberOfBlocks; i++) {
+    const partialKey = `${key}_${i}`;
+    const block = jsonstr.substr(i * blockSize, blockSize);
+    console.info("[syncStore:partial]", {
+      partialKey,
+      i,
+      start: i * blockSize,
+      blockSize,
+      block,
+    });
+    storageObj[partialKey] = block;
+  }
+
+  console.info("[syncStore:set]", storageObj);
+  chrome.storage.sync.set(storageObj, callback);
+}
+
+function syncGet(key, callback) {
+  (async () => {
+    try {
+      let i = 0;
+      let foundData;
+      while (true) {
+        let partialKey = `${key}_${i}`;
+        const found = await chrome.storage.sync.get(partialKey);
+        const data = found[partialKey];
+        console.info("[syncGet:partial]", { key: partialKey, data });
+        if (!data) {
+          break;
+        } else {
+          foundData = (foundData || "") + data;
+        }
+        i++;
+      }
+      console.info("[syncGet:get]", { key, foundData });
+      callback(foundData !== undefined ? JSON.parse(foundData) : foundData);
+    } catch (e) {
+      console.error("[syncGet:error]", key, e);
+      callback(undefined);
+    }
+  })();
+}
--- src/popup.js
+++ src/popup.js
@@ -1,48 +1,130 @@
 const LIST_EL = document.getElementById("list");
 const ADD_BTN = document.getElementById("add_btn");
+const IMPORT_BTN = document.getElementById("import_btn");
 
-chrome.storage.sync.get("liveInfo", ({ liveInfo }) => {
+syncGet("liveInfo", (liveInfo) => {
   LIST_EL.innerText = `Loading... (${(liveInfo || []).length} existing)`;
   setInfoBody(liveInfo);
 });
 
-async function addToFollows(type, channel) {
-  const { follows: existing } = (await chrome.storage.sync.get("follows")) || {
-    follows: [],
-  };
-  const follows = [...existing].concat({ type, channel });
-  chrome.storage.sync.set({
-    follows,
-  });
-  const { liveInfo } = await chrome.storage.sync.get("liveInfo");
+async function addToFollows(type, channels) {
+  console.info("[start:add]", type, channels);
+  let existing = [];
+  try {
+    existing = (await asyncGet("follows")) || [];
+  } catch (e) {
+    console.error(e);
+  }
+  const follows = [
+    ...new Map(
+      [...existing]
+        .concat(channels.map((channel) => ({ type, channel })))
+        .map((i) => [`${i.type}:${i.channel}`, i])
+    ).values(),
+  ];
+  console.info("[updated:add]", { existing, follows });
+  await asyncStore("follows", follows);
+  const liveInfo = await asyncGet("liveInfo");
+  setInfoBody(liveInfo);
+  console.info("[end:add]", type, channels);
+}
+
+async function removeFromFollows(type, channel) {
+  const existing = (await asyncGet("follows")) || [];
+  const follows = existing.filter(
+    (e) => !(e.type === type && e.channel === channel)
+  );
+  await asyncStore("follows", follows);
+  const liveInfo = await asyncGet("liveInfo");
   setInfoBody(liveInfo);
 }
 
 // When the button is clicked, inject getPage into current page
 ADD_BTN.addEventListener("click", async () => {
-  console.info("[click]");
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   const { url } = tab;
+  console.info("[click:add]", url);
   if (url.includes("twitch.tv")) {
     const urlObj = new URL(url);
     const parts = urlObj.pathname.split("/");
     const name = parts[1];
     if (name) {
-      addToFollows("twitch", name);
+      await addToFollows("twitch", [name]);
     }
   } else if (url.includes("youtube")) {
     const urlObj = new URL(url);
     const name = urlObj.searchParams.get("ab_channel");
     if (name) {
-      addToFollows("youtube", name);
+      await addToFollows("youtube", [name]);
     }
   }
 });
 
+IMPORT_BTN.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  console.info("[click:import]", tab);
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      func: function () {
+        if (window.location.href.startsWith("https://www.twitch.tv")) {
+          let hasNext = true;
+          do {
+            const nextBtn = document.querySelector(
+              '[data-test-selector="ShowMore"]'
+            );
+            hasNext = !!nextBtn;
+            if (nextBtn) nextBtn.click();
+          } while (hasNext);
+          const channels = [
+            ...document.querySelectorAll(
+              '[data-test-selector="followed-channel"]'
+            ),
+          ].map((e) => e.href.replace("https://www.twitch.tv/", ""));
+          return { type: "twitch", channels };
+        } else if (window.location.href.includes("youtube")) {
+          document.querySelectorAll("#expander-item").item(1).click();
+          const subTitle = [
+            ...document.querySelectorAll("#guide-section-title"),
+          ].find((e) => {
+            return e.innerText.toLowerCase() === "subscriptions";
+          });
+          try {
+            const itemsEl = subTitle.parentElement.nextSibling.nextSibling;
+            const channels = [...itemsEl.querySelectorAll("#endpoint")]
+              .map((i) => {
+                return decodeURIComponent(
+                  i.href
+                    .replace("https://www.youtube.com/c/", "")
+                    .replace("https://www.youtube.com/user/", "")
+                    .replace("https://www.youtube.com/feed/", "")
+                    .replace("https://www.youtube.com/", "")
+                    .trim()
+                );
+              })
+              .filter((c) => !c.startsWith("channel/"))
+              .filter(Boolean);
+            return { type: "youtube", channels };
+          } catch {
+            alert(
+              "To import, please go to Twitch or Youtube, and make sure the subscriptions list is visible on the left."
+            );
+          }
+        } else {
+          alert(
+            "To import, please go to Twitch or Youtube, and make sure the subscriptions list is visible on the left."
+          );
+        }
+      },
+    },
+    ([{ result }]) => {
+      addToFollows(result.type, result.channels);
+    }
+  );
+});
+
 async function setInfoBody(liveInfo) {
-  const { follows } = (await chrome.storage.sync.get("follows")) || {
-    follows: [],
-  };
+  const follows = (await asyncGet("follows")) || [];
   LIST_EL.innerText = "";
   const mappedFollows = follows.map((f) => ({
     ...f,
@@ -51,15 +133,29 @@ async function setInfoBody(liveInfo) {
   mappedFollows
     .sort(({ isLive }) => (isLive ? -1 : 1))
     .forEach(({ channel, isLive, link, type }) => {
-      const a = document.createElement("a");
-      a.classList.add("row");
-      a.innerHTML = `<span class="${type}">[${type}]</span><span style="flex: 2;">${channel}</span><span>${
-        isLive === undefined ? "???" : isLive ? "LIVE" : ""
-      }</span>`;
-      a.onclick = function () {
+      const rowEl = document.createElement("span");
+      rowEl.classList.add("row");
+      const typeEl = document.createElement("span");
+      typeEl.classList.add(type);
+      typeEl.innerHTML = `[${type}]`;
+      const channelEl = document.createElement("span");
+      channelEl.style = "flex: 2;";
+      channelEl.innerHTML = channel;
+      const liveEl = document.createElement("span");
+      liveEl.innerHTML = isLive === undefined ? "???" : isLive ? "LIVE" : "";
+      channelEl.onclick = function () {
         chrome.tabs.create({ active: true, url: link });
       };
-      LIST_EL.appendChild(a);
+      const delEl = document.createElement("button");
+      delEl.innerHTML = "Remove";
+      delEl.onclick = function () {
+        removeFromFollows(type, channel);
+      };
+      rowEl.appendChild(typeEl);
+      rowEl.appendChild(channelEl);
+      rowEl.appendChild(liveEl);
+      rowEl.appendChild(delEl);
+      LIST_EL.appendChild(rowEl);
     });
 }
 
@@ -70,3 +166,67 @@ chrome.runtime.onMessage.addListener(function (req, sender, sendResponse) {
 });
 
 chrome.runtime.sendMessage({ type: "request-update" });
+
+function asyncStore(key, objectToStore) {
+  return new Promise((resolve) => syncStore(key, objectToStore, resolve));
+}
+
+function asyncGet(key) {
+  return new Promise((resolve) => syncGet(key, resolve));
+}
+
+function syncStore(key, objectToStore, callback) {
+  const jsonstr = JSON.stringify(objectToStore);
+  const storageObj = {};
+  const blockSize =
+    chrome.storage.sync.QUOTA_BYTES_PER_ITEM / 2 - (4 + key.length);
+  const numberOfBlocks = Math.ceil(jsonstr.length / blockSize);
+  console.info("[syncStore:start]", {
+    key,
+    numberOfBlocks,
+    blockSize,
+    itemSize: jsonstr.length,
+  });
+
+  for (let i = 0; i < numberOfBlocks; i++) {
+    const partialKey = `${key}_${i}`;
+    const block = jsonstr.substr(i * blockSize, blockSize);
+    console.info("[syncStore:partial]", {
+      partialKey,
+      i,
+      start: i * blockSize,
+      blockSize,
+      block,
+    });
+    storageObj[partialKey] = block;
+  }
+
+  console.info("[syncStore:set]", storageObj);
+  chrome.storage.sync.set(storageObj, callback);
+}
+
+function syncGet(key, callback) {
+  (async () => {
+    try {
+      let i = 0;
+      let foundData;
+      while (true) {
+        let partialKey = `${key}_${i}`;
+        const found = await chrome.storage.sync.get(partialKey);
+        const data = found[partialKey];
+        console.info("[syncGet:partial]", { key: partialKey, data });
+        if (!data) {
+          break;
+        } else {
+          foundData = (foundData || "") + data;
+        }
+        i++;
+      }
+      console.info("[syncGet:get]", { key, foundData });
+      callback(foundData !== undefined ? JSON.parse(foundData) : foundData);
+    } catch (e) {
+      console.error("[syncGet:error]", { key }, e);
+      callback(undefined);
+    }
+  })();
+}
